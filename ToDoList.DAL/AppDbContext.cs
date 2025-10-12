a4e1f66b167fd06b3f11b5968757245f686cf780
Davins <danie2dickson3@gmail.com>
2025-10-13 00:37:13 +0500

    Create Dbset

--- ToDoList.DAL/AppDbContext.cs
+++ ToDoList.DAL/AppDbContext.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Entity;
 
 namespace ToDoList.DAL
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext() // Конструктор класса AppDbContext
+        {
+            Database.EnsureCreated(); // С помощью этого метода будет создоваться БД при обращении.
+        }
 
-    }
+        public DbSet<TaskEntity> Tasks { get; set; }
+
+    }   
 }
+
