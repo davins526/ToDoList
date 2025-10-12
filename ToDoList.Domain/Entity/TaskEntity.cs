a7f6eed03a90f297f00cbc88d69b396d44ea8709
Davins <danie2dickson3@gmail.com>
2025-10-13 00:08:53 +0500

    Create Entiy folder and class task

---
+++ ToDoList.Domain/Entity/TaskEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entity
+{
+    public class TaskEntity
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
