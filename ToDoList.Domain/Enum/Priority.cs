a6d7306055e15637cbc9053c548f92ef386df689
Davins <danie2dickson3@gmail.com>
2025-10-19 22:46:30 +0500

    Edit enum and add section Scripts

--- ToDoList.Domain/Enum/Priority.cs
+++ ToDoList.Domain/Enum/Priority.cs
@@ -1,10 +1,15 @@
-﻿namespace ToDoList.Domain.Enum
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum
 
 {
     public enum Priority
     {
+        [Display(Name = "Простая")]
         Easy = 1,
+        [Display(Name = "Важная")]
         Medium = 2,
+        [Display(Name = "Критичная")]
         Hard = 3
     }
 }
