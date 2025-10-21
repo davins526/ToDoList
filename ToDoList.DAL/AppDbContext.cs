1343a782b51bef8fce52da1ec5701b5a53e1bf9e
Davins <danie2dickson3@gmail.com>
2025-10-22 00:36:25 +0500

    DayFinExitSave

--- ToDoList.DAL/AppDbContext.cs
+++ ToDoList.DAL/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace ToDoList.DAL
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // Конструктор класса AppDbContext
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated(); // С помощью этого метода будет создоваться БД при обращении.
         }
 
